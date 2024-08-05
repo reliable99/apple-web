7b343b005a7b5a5a444fd4d2e4b3ed7fc498782d
relaible99 <abdulmuhminsodiq@gmail.com>
2024-08-05 14:03:22 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-blue-800 border-2 border-yellow-800 rounded-lg first-letter:text-white">
+      Hello world!
+    </h1>
+  );
+}
