4f15fd1185c04d05c60bac15e6374ea50d6383b8
relaible99 <abdulmuhminsodiq@gmail.com>
2024-08-06 12:25:21 +0100

    file structuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-blue-800 border-2 border-yellow-800 rounded-lg first-letter:text-white">
+    <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
   );
