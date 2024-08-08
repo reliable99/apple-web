baf53d2643db41d4f0885190e60df22b242b6837
relaible99 <abdulmuhminsodiq@gmail.com>
2024-08-08 20:28:14 +0100

    model

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from "./components/Model";
 const App = () => {
   return (
     <main className="bg-black">
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
 
       <Highlights />
+      <Model />
     </main>
   );
 };
