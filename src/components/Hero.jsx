0b2335022975e885574624769b04452b6b688c8f
relaible99 <abdulmuhminsodiq@gmail.com>
2024-08-19 20:54:13 +0100

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,7 +42,7 @@ const Hero = () => {
         id="cta"
         className="flex flex-col items-center opacity-0 translate-y-20"
       >
-        <a href="#highlights" className="btn">BUY</a>
+        <a href="#highlights" className="btn">buy</a>
         <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
